f013f15579367b8247517c65008e35de97b1e349
Hacker-fanan <114069247+Hacker-fanan@users.noreply.github.com>
2022-09-24 10:22:58 +0100

    Add files via upload

---
+++ 6.py
@@ -0,0 +1,32 @@
+from time import sleep as timeout
+import time
+import base64
+import socket
+import sys
+import os
+os.system ('clear')
+def main (main):
+    print ('')
+    time.sleep(0.2)
+    print ("\033[1;36m...ﺮﻈﺘﻧﺍ")
+    time.sleep(4)
+    print ('\033[1;32m')
+    os.system ('clear')
+    print ("██╗░░██╗██╗░░██╗░░░░░░██████╗░░█████╗░░█████╗░░██████╗░██╗░░░░░███████╗")
+    print ("██║░░██║██║░██╔╝░░░░░██╔════╝░██╔══██╗██╔══██╗██╔════╝░██║░░░░░██╔════╝")
+    print ("███████║█████═╝░░░░░░██║░░██╗░██║░░██║██║░░██║██║░░██╗░██║░░░░░█████╗░░")
+    print ("██╔══██║██╔═██╗░░░░░░██║░░╚██╗██║░░██║██║░░██║██║░░╚██╗██║░░░░░██╔══╝░░")
+    print ("██║░░██║██║░╚██╗░░░░░╚██████╔╝╚█████╔╝╚█████╔╝╚██████╔╝███████╗███████╗")
+    print ("╚═╝░░╚═╝╚═╝░░╚═╝░░░░░░╚═════╝░░╚════╝░░╚════╝░░╚═════╝░╚══════╝╚══════╝")
+    time.sleep(0.2)
+    fx9 = input (" \033[1;33m @ نودﺑ ﺔﯾﺤﺿ بﺎﺴﺤ مﺴإ  ==> \033[1;32m")
+    time.sleep(0.2)
+    if fx9 == 'ettaherzouhair' :
+            os.system ('python g.py')
+            restart ()
+    elif fx9 == 'zouhairettaher2020' :
+            os.system ('python gg.py')
+    elif fx9 == 'zouhairettaher2022' :
+            os.system ('python ggg.py')
+main (main)
+home_main ()
